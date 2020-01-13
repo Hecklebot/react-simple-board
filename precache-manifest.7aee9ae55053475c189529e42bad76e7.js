2e64150569ab78d3523705f2e2e79500fcbdcfa3
Hecklebot <iuehakdrk@gmail.com>
2020-01-13 16:08:44 +0900

    Updates

---
+++ precache-manifest.7aee9ae55053475c189529e42bad76e7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ecfed6408aff6ae0d583ec5cc6fe8116",
+    "url": "/react-simple-board/index.html"
+  },
+  {
+    "revision": "09427764458006fe4890",
+    "url": "/react-simple-board/static/css/2.35f98baf.chunk.css"
+  },
+  {
+    "revision": "21c5e9289a5820413085",
+    "url": "/react-simple-board/static/css/main.5d7ec4ed.chunk.css"
+  },
+  {
+    "revision": "09427764458006fe4890",
+    "url": "/react-simple-board/static/js/2.e84b83e5.chunk.js"
+  },
+  {
+    "revision": "87984cad984dae3222b2a1d0861003a4",
+    "url": "/react-simple-board/static/js/2.e84b83e5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "21c5e9289a5820413085",
+    "url": "/react-simple-board/static/js/main.251ee25d.chunk.js"
+  },
+  {
+    "revision": "0f64233f574c914f22f4",
+    "url": "/react-simple-board/static/js/runtime-main.d83bbe97.js"
+  }
+]);
